f11176ff4e9110b38ef6218e8856f0d649695be8
Gerred Gruenspan <ggruenspan@outlook.com>
2023-10-27 20:19:08 -0400

    Updated Sign In & Up

--- client/src/app/sign-in/sign-in.component.ts
+++ client/src/app/sign-in/sign-in.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
+  passwordVisible: boolean = false;
+  showPasswordIcon: string = 'fa-eye-slash';
 
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+    this.showPasswordIcon = this.passwordVisible ? 'fa-eye' : 'fa-eye-slash';
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = this.passwordVisible ? 'text' : 'password';
+    }
+  }
+
+  signInSubmit() {
+    console.log('signInSubmit');
+  }
 }
--- client/src/app/sign-up/sign-up.component.ts
+++ client/src/app/sign-up/sign-up.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  passwordVisible: boolean = false;
+  showPasswordIcon: string = 'fa-eye-slash';
 
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+    this.showPasswordIcon = this.passwordVisible ? 'fa-eye' : 'fa-eye-slash';
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    if (passwordInput) {
+      passwordInput.type = this.passwordVisible ? 'text' : 'password';
+    }
+  }
+
+  signUpSubmit() {
+    console.log('signUpSubmit');
+  }
 }
