2740b757c08fa4b7f1c7060527654e3ebcb00ea1
Gerred Gruenspan <ggruenspan@outlook.com>
2023-10-28 21:25:52 -0400

    Added Comments

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -11,10 +11,12 @@ export class AccountService {
 
   constructor(private http: HttpClient) {}
 
+  // Method for sending a sign-up request to the API
   signUp(data: any): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/sign-up`, data);
   }
 
+  // Method for sending a sign-in request to the API
   signIn(data: any): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/sign-in`, data);
   }
