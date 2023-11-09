3a106dd259dcd1bb14572a630a0b44594fd72aad
Gerred Gruenspan <ggruenspan@outlook.com>
2023-11-08 23:23:59 -0500

    Updated Interceptor

--- client/src/app/http-interceptors/auth.interceptor.ts
+++ client/src/app/http-interceptors/auth.interceptor.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
+import { LocalStorageService } from '../services/localStorage.service';
 
 @Injectable()
 
 export class HttpInterceptorService implements HttpInterceptor {
 
-constructor() { }
+constructor(private LocalStorageService: LocalStorageService) { }
 
 intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const token = localStorage.getItem('jwtToken');
+    const token = this.LocalStorageService.get('jwtToken');
     req = req.clone({
         url:  req.url,
         setHeaders: {
