e30a3b05b5dea1999dab3f8a30bf410e654394a2
Gerred Gruenspan <ggruenspan@outlook.com>
2024-08-20 12:41:08 -0400

    feat(home): redesign home component layout with enhanced content and placeholders
    - Replaced the basic "home works!" text with a fully structured layout in `home.component.html`.
    - Introduced a new main container with text content highlighting the benefits of personalized tutoring.
    - Added a dynamic search bar with placeholder inputs, conditional rendering based on `changeSearchBar`.
    - Implemented an image section that conditionally displays user avatars with varying sizes.
    - These placeholders serve as a template for future dynamic content integration.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+// import { TutorRoutesService } from '../../services/routes/tutorRoutes.service';
+// import { HandleDataService } from '../../services/handleData.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit  {
+  isScreenLess = false;
+  changeSearchBar = false;
 
+  // constructor(private tutorService: TutorRoutesService) {}
+
+  ngOnInit() {
+    this.handleWindowResize(); // Initialize window resize handling
+    // this.handleTutorData(); // Initialize tutor data handling
+  }
+
+  // Listens to window resize event to handle changes in screen width
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.handleWindowResize();
+  }
+
+  // Handles changes in window size
+  private handleWindowResize() {
+    if (window.innerWidth >= 992) {
+      this.isScreenLess = true;
+    } else { 
+      this.isScreenLess = false;
+    }
+
+    if (window.innerWidth >= 481) {
+      this.changeSearchBar = true;
+    } else { 
+      this.changeSearchBar = false;
+    }
+  }
+
+
+  // Handles user data from API
+  // handleTutorData() {};
 }
