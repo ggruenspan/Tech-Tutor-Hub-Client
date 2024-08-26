809f189f0cb0cb861db4790f4d260485bd192785
Gerred Gruenspan <ggruenspan@outlook.com>
2024-08-26 00:22:15 -0400

    Redesign role.guard.ts for improved access control
    - Refactored the role.guard.ts file to enhance access control logic and security.
    - Updated role checks and permissions to better align with application requirements.
    - Improved code readability and maintainability.
    - Ensured compatibility with recent changes in authentication and authorization mechanisms.
    
    This redesign provides more robust and flexible role-based access control.

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -11,19 +11,15 @@ export class RoleGuardService implements CanActivate {
   constructor(private dataService: HandleDataService, private route: Router, private toastr: ToastrService) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    const requiredRole = route.data['requiredRole']; // Accessing route data
-    const localStorageRole = localStorage.getItem('role');
+    const requiredRole = route.data['requiredRole'];
     const tokenRole = this.dataService.getRoleFromToken(this.dataService.getDecodedToken());
 
-    console.log(localStorageRole);
-    console.log(tokenRole);
-
-    if (tokenRole.includes(requiredRole) && localStorageRole?.includes(requiredRole)) {
-      return true; // Roles match, allow access
+    if (tokenRole === requiredRole) {
+        return true; // Roles match, allow access
     } else {
-      this.toastr.error('Access denied');
-      this.route.navigate(['/']);
-      return false; // Roles do not match, deny access
+        this.toastr.error('Access denied');
+        this.route.navigate(['/']);
+        return false; // Roles do not match, deny access
     }
-  }
+}
 }
