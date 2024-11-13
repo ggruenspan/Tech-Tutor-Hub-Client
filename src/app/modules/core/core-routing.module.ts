9a9a6e9727416a62b271f51490f5169dff135031
Gerred Gruenspan <ggruenspan@outlook.com>
2024-11-13 14:25:36 -0500

    feat(core): added community to nav
    - community page will host user fourms / discussion

--- src/app/modules/core/core-routing.module.ts
+++ src/app/modules/core/core-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FindYourTutorComponent } from '../../components/find-your-tutor/find-your-tutor.component';
 import { BecomeATutorComponent } from '../../components/become-a-tutor/become-a-tutor.component';
+import { CommunityComponent } from '../../components/community/community.component';
 import { HelpComponent } from '../../components/help/help.component';
 import { PageNotFoundComponent } from '../../components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'find-your-tutor', component: FindYourTutorComponent },
   { path: 'become-a-tutor', component: BecomeATutorComponent },
+  { path: 'community', component: CommunityComponent},
   { path: 'help', component: HelpComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
