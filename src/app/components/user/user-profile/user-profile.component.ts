6ad62fccdd864e5d6471fbd1421863636ccaf6e4
Gerred Gruenspan <ggruenspan@outlook.com>
2024-11-13 14:23:38 -0500

    feat(user): add basic code to retrieve user data

--- src/app/components/user/user-profile/user-profile.component.ts
+++ src/app/components/user/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserRoutesService } from '../../../services/routes/userRoutes.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
+  userName: string = '';
+  userProfile: any;
 
+  constructor(private route: ActivatedRoute, private userRoutes: UserRoutesService) {}
+
+  ngOnInit(): void {
+    // Get the username from the route
+    this.userName = this.route.snapshot.paramMap.get('userName') || '';
+
+    // Fetch the user profile
+    this.userRoutes.getUserProfile(this.userName).subscribe((data) => {
+      this.userProfile = data.user;
+      console.log(data);
+    });
+  }
 }
